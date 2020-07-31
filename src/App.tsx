46806c8837f8b3b1ff1d42e427975e8b6cb099a4
Ranisans <ranisans@yahoo.com>
2020-07-31 15:23:03 +0300

    feat: add showing error window if authentication server is down

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,42 @@ import "./main.scss";
 
 const App: React.FC = () => {
   const [isAuth, setIsAuth] = React.useState(false);
+  const [message, setMessage] = React.useState<string>("");
   const popUpRef = React.createRef<HTMLDivElement>();
 
-  const logIn = async (login: string, password: string) => {
-    const response = await fetch(`${connectUrl}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: login, password }),
-    });
-    const result = await response.json();
-    if (result.accessToken) {
-      setIsAuth(true);
-    } else {
+  const serverErrorMessage = "Server error!";
+  const authenticationErrorMessage = "Wrong Login or Password!";
+
+  React.useEffect(() => {
+    if (message !== "") {
       popUpRef.current?.classList.remove("pop_up--hidden");
       setTimeout(() => {
         popUpRef.current?.classList.add("pop_up--hidden");
+        setTimeout(() => {
+          setMessage("");
+        }, 500);
       }, 1500);
+    }
+  }, [message, popUpRef]);
+
+  const logIn = async (login: string, password: string) => {
+    try {
+      const response = await fetch(`${connectUrl}/login`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: login, password }),
+      });
+      const result = await response.json();
+      if (result.accessToken) {
+        setIsAuth(true);
+      } else {
+        setMessage(authenticationErrorMessage);
+      }
+    } catch (e) {
+      setMessage(serverErrorMessage);
     }
   };
 
@@ -81,7 +99,7 @@ const App: React.FC = () => {
           Component={ContactPage}
         />
         <div className="pop_up pop_up--hidden" ref={popUpRef}>
-          <div className="pop_up-message">Wrong Login or Password</div>
+          <div className="pop_up-message">{message}</div>
         </div>
       </div>
     </Router>
